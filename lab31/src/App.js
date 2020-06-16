f218c4ad5607756e23a3e66f70708f40715d013e
sisidjidji <s.azibi@live.fr>
2020-06-15 21:39:44 -0500

    main componant done

--- lab31/src/App.js
+++ lab31/src/App.js
@@ -10,15 +10,48 @@ function Header() {
 }
 
 function Footer(){
- return(<div>
+ return(
+  <div>
     &copy; DeltaV 2020 
   </div>)
 }
+
+function Counter(){
+  return (
+<div>
+  
+  <button onClick={this.HandlerCount} >+</button>
+  <button>-</button>
+</div>
+  )
+}
+
+class Main extends React.Component{
+  constructor(prop){
+    super(prop);
+    this.state={
+words: count
+    };
+  }
+
+  HandlerCount= e=>{
+    e.preventDefault();
+    let count= e.target.value
+  this.setState({count})  
+}
+
+  render(
+  
+  )
+}
 class App extends React.Component{
+  
   render(){
     return(
-      <div class="wrapper">
+      <div className="wrapper">
         <Header/>
+        <Main/>
+        <Counter/>
         <Footer/>
       </div>
     )
